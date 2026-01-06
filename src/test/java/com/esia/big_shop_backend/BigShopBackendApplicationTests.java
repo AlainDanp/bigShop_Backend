5b097d40d6fb959a31c1ca10e0c3ef1dfed46f59
Alain <datouondjoubi@et.esiea.fr>
2026-01-06 17:08:04 +0100

    first commit biash

---
+++ src/test/java/com/esia/big_shop_backend/BigShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esia.big_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
